a6c21e5e93badd05db3cb18bed033a2d4fcde21b
Huzaifa <HuzaifaIslamRakib@gmail.com>
2025-01-17 16:02:00 +0600

    made update role verifed and make hr api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
@@ -27,16 +27,126 @@ async function run() {
     await client.connect();
     const workify = client.db("workify");
     const users = workify.collection("users");
+    const tasks = workify.collection("tasks");
+    const payments = workify.collection("payments");
     // get all users
     app.get("/users", async (req, res) => {
-      const result = await users.find().toArray();
+      const isAdmin = req?.query?.admin;
+      let result;
+      let filter = {};
+      let options = {
+        sort: { created: -1 },
+      };
+      if (Boolean(isAdmin)) {
+        result = await users.find(filter, options).toArray();
+      } else {
+        filter = {
+          role: "Employee",
+        };
+        result = await users.find(filter, options).toArray();
+      }
+      res.send(result);
+    });
+
+    // change verification for employee
+    app.patch("/updateverified/:id", async (req, res) => {
+      const id = req?.params.id; // Get the document ID from query parameters
+      const filter = { _id: new ObjectId(id) };
+      const user = await users.findOne(filter);
+      const verified = user?.verified;
+      const update = { $set: { verified: !verified } };
+      const result = await users.updateOne(filter, update);
+      res.send(result);
+    });
+
+    // change role for employee
+    app.patch("/updaterole/:id", async (req, res) => {
+      const id = req?.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const user = await users.findOne(filter);
+      let role = user?.role;
+      if (role == "HR") {
+        role = "Employee";
+      } else {
+        role = "HR";
+      }
+      const update = { $set: { role: role } };
+      const result = await users.updateOne(filter, update);
+      res.send(result);
+    });
+
+    // change fire for employee
+    app.patch("/updatefired/:id", async (req, res) => {
+      const id = req?.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const user = await users.findOne(filter);
+      let fire = user?.fired;
+      if (fire == "True") {
+        fire = "False";
+      } else {
+        fire = "True";
+      }
+      const update = { $set: { fired: fire } };
+      const result = await users.updateOne(filter, update);
       res.send(result);
     });
 
     // post a user
     app.put("/adduser", async (req, res) => {
-      const user = req.body;
-      const result = await users.insertOne(user);
+      const user = req?.body;
+      const options = { upsert: true };
+      const filter = { email: user.email };
+      const result = await users.updateOne(filter, { $set: user }, options);
+      res.send(result);
+    });
+
+    //get a task by email
+    app.get("/owntask", async (req, res) => {
+      const email2 = req?.query?.email;
+      const filter = {
+        email: email2,
+      };
+      const options = {
+        sort: { created: -1 },
+      };
+      const result = await tasks.find(filter, options).toArray();
+      res.send(result);
+    });
+
+    // update a task
+    app.put("/owntask/:id", async (req, res) => {
+      const id = req?.params?.id;
+      const updatedTask = req?.body;
+      const filter = { _id: new ObjectId(id) };
+      const result = await tasks.updateOne(filter, { $set: updatedTask });
+      res.send(result);
+    });
+
+    //delete a task
+    app.delete("/owntask/:id", async (req, res) => {
+      const id = req?.params?.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await tasks.deleteOne(filter);
+      res.send(result);
+    });
+
+    //post a task
+    app.post("/addtask", async (req, res) => {
+      const newTask = req?.body;
+      const result = await tasks.insertOne(newTask);
+      res.send(result);
+    });
+
+    //get a task by email
+    app.get("/ownpayment", async (req, res) => {
+      const email2 = req?.query?.email;
+      const filter = {
+        email: email2,
+      };
+      const options = {
+        sort: { created: -1 },
+      };
+      const result = await payments.find(filter, options).toArray();
       res.send(result);
     });
 
